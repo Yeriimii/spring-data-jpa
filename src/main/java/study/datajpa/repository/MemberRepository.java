dd36e49ca6065b7d6bcfe6d10781c747fda39654
Yeriimii <seojs0511@naver.com>
2023-10-04 10:05:55 +0900

    @Query, 리포지토리 메소드에 쿼리 정의하기

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -15,4 +15,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
 //    @Query(name = "Member.findByUsername") // NamedQuery
     List<Member> findByUsername(@Param("username") String username);  // 네임드 쿼리를 우선으로 찾는다.
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
